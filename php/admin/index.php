b1a038e0beeac22c871835cf1661aced83ac4ef3
calel95 <borgescalel@gmail.com>
2022-12-15 02:45:47 +0000

    versao inicial

---
+++ php/admin/index.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+<title>Projeto Docker 01</title>
+</head>
+
+
+<?php
+ini_set("display_errors", 1);
+header('Content-Type: text/html; charset=iso-8859-1');
+
+
+
+echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+
+$servername = "mysqlsrv";
+$username = "root";
+$password = "Senha123";
+$database = "testedb";
+
+// Criar conexão
+
+
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$query = "SELECT * FROM Cadastro";
+
+if ($result = mysqli_query($link, $query)) {
+
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+        printf ("%s %s %s %s <br>", $row["Id"], $row["Nome"], $row["idade"], $row["cpf"]);
+    }
+
+    
+    mysqli_free_result($result);
+}
+
+
+mysqli_close($link);
+
+?>
+
+</html>
+
